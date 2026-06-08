ce317941bd2eb706aa23cff6d9a622335099b317
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-09 01:22:31 +0530

    Time: 5 ms (66.43%) | Memory: 14.8 MB (37.07%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        // int freq[256]={0};
-        // for(char ch:s){
-        //     freq[ch]++;
-        // }
-        // for(int i=0;i<s.size();i++){
-        //     if(freq[s[i]]==1){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-        unordered_map<char,int>m;
-        queue<int>q;
+        int freq[256]={0};
+        for(char ch:s){
+            freq[ch]++;
+        }
         for(int i=0;i<s.size();i++){
-            if(m.find(s[i]) == m.end()){
-                q.push(i);
-            }
-            m[s[i]]++;
-
-            while(q.size()>0 && m[s[q.front()]]>1){
-                q.pop();
+            if(freq[s[i]]==1){
+                return i;
             }
         }
-        if(q.empty())
-            return -1;
-        return q.front();
+        return -1;
+        // unordered_map<char,int>m;
+        // queue<int>q;
+        // for(int i=0;i<s.size();i++){
+        //     if(m.find(s[i]) == m.end()){
+        //         q.push(i);
+        //     }
+        //     m[s[i]]++;
+
+        //     while(q.size()>0 && m[s[q.front()]]>1){
+        //         q.pop();
+        //     }
+        // }
+        // if(q.empty())
+        //     return -1;
+        // return q.front();
     }
 };
