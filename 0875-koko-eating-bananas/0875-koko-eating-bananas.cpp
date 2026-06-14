3f1fa81f445bdbb01c330c6acecfa7e9321c73dc
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-14 16:41:55 +0530

    0875-koko-eating-bananas

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canEat(vector<int>&piles,int h, int speed){
+        int totalhrs=0;
+        int n=piles.size();
+        for(int i=0;i<n;i++){
+            totalhrs+=ceil((double)piles[i]/(double)speed);
+            if(totalhrs>h){
+                return false;
+            }
+        }
+        return totalhrs<=h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(canEat(piles,h,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
