41fdcae36539a32d8dedfde55dc7d04701743ad4
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-24 00:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (81.43%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n=nums.size();
+        int curr_sum=nums[0];
+        int max_sum=nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                curr_sum+=nums[i];
+            }
+            else{
+                max_sum=max(max_sum,curr_sum);
+                curr_sum=nums[i];
+            }
+        }
+        max_sum=max(max_sum,curr_sum);
+        return max_sum;
+    }
+};
