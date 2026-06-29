2c8e69cd1a868415f590e6ee373bd8e20da0d5d8
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-30 01:43:52 +0530

    Time: 3 ms (74.49%) | Memory: 18.4 MB (85.54%) - LeetSync

---
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                char sm=min(s[i],s[j]);
+                s[i]=sm;
+                s[j]=sm;
+            }
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
