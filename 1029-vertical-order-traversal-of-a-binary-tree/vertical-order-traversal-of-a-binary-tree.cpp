d33c216a156dfb0316412ff8969c45d8b3a05236
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-07-02 00:23:16 +0530

    Time: 1 ms (57.41%) | Memory: 15.9 MB (66.16%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+
+        map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*, pair<int,int>>> q;
+
+        q.push({root,{0,0}}); // node, {row,col}
+
+        while(!q.empty()){
+
+            auto temp = q.front();
+            q.pop();
+
+            TreeNode* node = temp.first;
+            int row = temp.second.first;
+            int col = temp.second.second;
+
+            mp[col][row].insert(node->val);
+
+            if(node->left)
+                q.push({node->left,{row+1,col-1}});
+
+            if(node->right)
+                q.push({node->right,{row+1,col+1}});
+        }
+
+        vector<vector<int>> ans;
+
+        for(auto &col : mp){
+
+            vector<int> vertical;
+
+            for(auto &row : col.second){
+
+                vertical.insert(vertical.end(),
+                                row.second.begin(),
+                                row.second.end());
+            }
+
+            ans.push_back(vertical);
+        }
+
+        return ans;
+    }
+};
