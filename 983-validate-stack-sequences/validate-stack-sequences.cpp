a9a2beeee1e655bf1857f4cb14d06a1e5eaf2aa3
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-07-03 23:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (47.69%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int>st;
+        int j=0;
+        for(int ele:pushed){
+            st.push(ele);
+            while(!st.empty() && j<popped.size() && st.top() == popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty();
+
+    }
+};
