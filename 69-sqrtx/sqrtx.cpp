b6357f2888b32459d6235385207c3f0ae8d4d206
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-07-07 22:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.01%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        int ans = 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(1LL * mid * mid == x)
+                return mid;
+            if(1LL * mid * mid >x)
+                high = mid -1;
+            else{
+                ans = mid;
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+};
